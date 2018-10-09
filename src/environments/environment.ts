f97309469b156b01e6bab3d2f83e45e02b1ea857
Yuri All Doctors <yuri@alldoctors.com.br>
2018-10-09 13:53:14 -0300

    Create

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+    apiKey: "AIzaSyBIVNcUn_OY4xtUXlUAB2TuQ6_pu6D5WOI",
+    authDomain: "teste-29d73.firebaseapp.com",
+    databaseURL: "https://teste-29d73.firebaseio.com/",
+    projectId: "teste-29d73",
+    storageBucket: "gs://teste-29d73.appspot.com",
+    messagingSenderId: "340515382596"
   }
 };
